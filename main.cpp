ce5725809516c36ce996bafe82e1bc425137cfdb
Kiritow <1362050620@qq.com>
2017-03-27 23:19:48 +0800

    Add JudgeClient and UserClient

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
 #include "gsock.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <memory>
+using namespace std;
+
+class JudgeClient
+{
+public:
+
+private:
+    shared_ptr<sock> s;
+};
+
+class UserClient
+{
+public:
+
+private:
+    shared_ptr<sock> s;
+};
 
 int main()
 {
