a5be15deadc5512be7e4b030f2390eb3061e6198
Kiritow <1362050620@qq.com>
2017-04-01 09:44:38 +0800

    Update Example Code Add Services and ServerListeners.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,58 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <thread>
 #include <mutex>
+#include <future>
 using namespace std;
 
+/// JudgeClient(Yui-Friend) --- JudgeServer(Yui-Home)
 #define JUDGE_SERVER_PORT 60001
+
+/// UserClient(Yui) --- JudgeServer(Yui-Home)
+#define USER_SERVER_PORT 60002
 
 void ShowWelcomeMessage()
 {
-    cout<<"Welcome to use Programing contest controller system! I'm Yui!"<<endl;
-    cout<<"Now I'll serve the contest as soon as possible!"<<endl;
-    cout<<endl;
-    cout<<"Yui-Home : Yui-Project Judge Network Server."<<endl;
-    cout<<"Yui-Project 2017 / HC TECH 2017"<<endl;
-    cout<<endl;
+    printf(
+    "Welcome to use Programing contest controller system! I'm Yui!\n"
+    "Now I'll serve the contest as soon as possible!\n"
+    "Yui-Home : Yui-Project Judge Network Server.\n"
+    "Yui-Project 2017 / HC TECH 2017\n"
+    );
+}
+
+
+bool stopJudgeClientLabel;
+
+void procJudgeClient(ServerListener& nsl)
+{
+    while(1)
+    {
+        if(stopJudgeClientLabel) break;
+        Connection c=nsl.accept();
+        if(c.ready())
+        {
+
+        }
+    }
+    return ;
+}
+
+bool stopUserClientLabel;
+
+void procUserClient(ServerListener& usl)
+{
+    return ;
+}
+
+char linecommand[1024];
+void ControlTerminal()
+{
+    while(1)
+    {
+        memset(linecommand,0,1024);
+        gets(linecommand);
+    }
 }
 
 int main()
@@ -27,12 +65,38 @@ int main()
 
     ShowWelcomeMessage();
 
-    log("Starting Server Socket...");
+    log("Starting ServerListener For Yui-Friend...");
     ServerListener nsl(JUDGE_SERVER_PORT,100);
     if(!nsl.ready())
     {
         log("Failed to start ServerSocket.");
+        return 0;
     }
+    log("ServerListener Started.");
+    log("Starting Service For Yui-Friend...");
 
+    stopJudgeClientLabel=false;
+    thread td(procJudgeClient,ref(nsl));
+    td.detach();
+
+    log("Service Started.");
+    log("Starting ServerListener For Yui...");
+    ServerListener usl(USER_SERVER_PORT,100);
+    if(!usl.ready())
+    {
+        log("Failed to start ServerSocket.");
+        return 0;
+    }
+    log("ServerListener Started.");
+    log("Starting Service For Yui...");
+
+    stopUserClientLabel=false;
+    thread tdx(procUserClient,ref(usl));
+    tdx.detach();
+
+    log("Service Started.");
+    log("Entering Control Terminal...");
+
+    ControlTerminal();
     return 0;
 }
